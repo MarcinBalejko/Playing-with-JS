65879c45c61ef5adf9ededa93034ce8e834c2d47
MarcinBalejko <mbbm9@o2.pl>
2020-02-20 19:02:29 +0100

    :white_check_mark::alembic: AJAX and JSON exercise

---
+++ ajax_json_exercise/js/main.js
@@ -0,0 +1,58 @@
+let pageCounter = 1;
+const animalContainer = document.getElementById("animal-info");
+const btn = document.getElementById("btn");
+
+
+btn.addEventListener('click', function () {
+    const ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', `https://learnwebcode.github.io/json-example/animals-${pageCounter}.json`);
+    ourRequest.onload = function () {
+        if (ourRequest.status >= 200 && ourRequest.status < 400) {
+            let ourData = JSON.parse(ourRequest.responseText);
+            renderHTML(ourData);
+        } else {
+            console.log("We connected to the server, but it returned an error.");
+        }
+
+    };
+    ourRequest.onerror = function () {
+        console.log("Connection error")
+    };
+    ourRequest.send();
+    pageCounter++;
+    if (pageCounter > 3) {
+        btn.classList.add("hide-me");  // css
+    }
+});
+
+function renderHTML(data) {
+    let htmlString = '';
+
+    for (i = 0; i < data.length; i++) {
+        htmlString += `<p> ${data[i].name} is a ${data[i].species} that likes to eat `;
+
+        for (ii = 0; ii < data[i].foods.likes.length; ii++) {
+            if (ii == 0) {
+                htmlString += data[i].foods.likes[ii];  // first element in foods.likes
+            } else {
+                htmlString += ` and ${data[i].foods.likes[ii]}`;
+            }
+        }
+
+        htmlString += ' and dislikes ';
+
+        for (ii = 0; ii < data[i].foods.dislikes.length; ii++) {
+            if (ii == 0) {
+                htmlString += data[i].foods.dislikes[ii];  // first element in foods.dislikes
+            } else {
+                htmlString += ` and ${data[i].foods.dislikes[ii]}`;
+            }
+        }
+
+        htmlString += '.</p>'
+    }
+
+    animalContainer.insertAdjacentHTML('beforeend', htmlString);
+}
+
+
