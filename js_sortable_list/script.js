28d8e6df63ce3031c251191a0ae86f709744b3c1
MarcinBalejko <mbbm9@o2.pl>
2020-07-20 23:50:08 +0200

    :white_check_mark::zap::sparkles: Added drag & drop functionality

--- js_sortable_list/script.js
+++ js_sortable_list/script.js
@@ -30,8 +30,6 @@ function createList() {
     })
     .map((a) => a.value) // <-- turning it back into an array of strings without 'sort:'
     .forEach((person, index) => {
-      console.log(person);
-
       const listItem = document.createElement("li");
 
       listItem.setAttribute("data-index", index);
@@ -51,15 +49,37 @@ function createList() {
   addEventListeners();
 }
 
-function dragStart() {}
+function dragStart() {
+  dragStartIndex = +this.closest("li").getAttribute("data-index");
+  console.log(dragStartIndex);
+}
 
-function dragEnter() {}
+function dragEnter() {
+  this.classList.add("over");
+}
 
-function dragLeave() {}
+function dragLeave() {
+  this.classList.remove("over");
+}
 
-function dragOver() {}
+function dragOver(e) {
+  e.preventDefault();
+}
 
-function dragDrop() {}
+function dragDrop() {
+  const dragEndIndex = +this.getAttribute("data-index");
+  swapItems(dragStartIndex, dragEndIndex);
+
+  this.classList.remove("over");
+}
+
+function swapItems(fromIndex, toIndex) {
+  const itemOne = listItems[fromIndex].querySelector(".draggable");
+  const itemTwo = listItems[toIndex].querySelector(".draggable");
+
+  listItems[fromIndex].appendChild(itemTwo);
+  listItems[toIndex].appendChild(itemOne);
+}
 
 function addEventListeners() {
   const draggables = document.querySelectorAll(".draggable");
