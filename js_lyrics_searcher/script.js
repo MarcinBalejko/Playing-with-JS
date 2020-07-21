b396449a962c08537759e701d9ef3e881b1ff2b7
MarcinBalejko <mbbm9@o2.pl>
2020-07-21 18:43:41 +0200

    :recycle::zap: Added prev and next buttons

--- js_lyrics_searcher/script.js
+++ js_lyrics_searcher/script.js
@@ -19,11 +19,65 @@ const apiURL = "https://api.lyrics.ovh";
 // ASYNC AWAIT method
 async function searchSongs(term) {
   const res = await fetch(`${apiURL}/suggest/${term}`);
-
   const data = await res.json();
 
-  console.log(data);
+  showData(data);
 }
+
+// Show song and artist in DOM
+function showData(data) {
+  // LONGER WAY ----------------------------
+  //   let output = "";
+
+  //   data.data.forEach((song) => {
+  //     output += `
+  //         <li>
+  //         <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+  //         <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+  //         </li>
+  //         `;
+  //   });
+
+  //   result.innerHTML = `
+  //   <ul class="songs">
+  //     ${output}
+  //   </ul>
+  //   `;
+  //-------------------------------------------
+
+  // SHORTER WAY
+  result.innerHTML = `
+    <ul class="songs">
+    ${data.data
+      .map(
+        (song) => `<li>
+        <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+        <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+        </li>`
+      )
+      .join("")}
+    </ul>
+`;
+
+  if (data.prev || data.next) {
+    more.innerHTML = `
+    ${
+      data.prev
+        ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+        : ""
+    }
+    ${
+      data.next
+        ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Next</button>`
+        : ""
+    }
+    `;
+  } else {
+    more.innerHTML = "";
+  }
+}
+
+function getMoreSongs(url) {}
 
 // Event listeners
 form.addEventListener("submit", (e) => {
