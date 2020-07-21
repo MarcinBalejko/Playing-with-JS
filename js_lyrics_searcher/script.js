3bbd067c883fde4cd3f129805e875a6a6cc59ed0
MarcinBalejko <mbbm9@o2.pl>
2020-07-21 21:49:37 +0200

    :white_check_mark: Fetching and displaying song lyrics

--- js_lyrics_searcher/script.js
+++ js_lyrics_searcher/script.js
@@ -6,17 +6,6 @@ const more = document.getElementById("more");
 const apiURL = "https://api.lyrics.ovh";
 
 // Search by song or artist
-
-//THEN. method----------------------------------------
-
-// function searchSongs(term) {
-//   fetch(`${apiURL}/suggest/${term}`)
-//     .then((res) => res.json())
-//     .then((data) => console.log(data));
-// }
-//------------------------------------------
-
-// ASYNC AWAIT method
 async function searchSongs(term) {
   const res = await fetch(`${apiURL}/suggest/${term}`);
   const data = await res.json();
@@ -26,51 +15,31 @@ async function searchSongs(term) {
 
 // Show song and artist in DOM
 function showData(data) {
-  // LONGER WAY ----------------------------
-  //   let output = "";
-
-  //   data.data.forEach((song) => {
-  //     output += `
-  //         <li>
-  //         <span><strong>${song.artist.name}</strong> - ${song.title}</span>
-  //         <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
-  //         </li>
-  //         `;
-  //   });
-
-  //   result.innerHTML = `
-  //   <ul class="songs">
-  //     ${output}
-  //   </ul>
-  //   `;
-  //-------------------------------------------
-
-  // SHORTER WAY
   result.innerHTML = `
     <ul class="songs">
-    ${data.data
-      .map(
-        (song) => `<li>
-        <span><strong>${song.artist.name}</strong> - ${song.title}</span>
-        <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
-        </li>`
-      )
-      .join("")}
+      ${data.data
+        .map(
+          (song) => `<li>
+      <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+      <button class="btn" data-artist="${song.artist.name}" data-songtitle="${song.title}">Get Lyrics</button>
+    </li>`
+        )
+        .join("")}
     </ul>
-`;
+  `;
 
   if (data.prev || data.next) {
     more.innerHTML = `
-    ${
-      data.prev
-        ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
-        : ""
-    }
-    ${
-      data.next
-        ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
-        : ""
-    }
+      ${
+        data.prev
+          ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+          : ""
+      }
+      ${
+        data.next
+          ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+          : ""
+      }
     `;
   } else {
     more.innerHTML = "";
@@ -85,6 +54,19 @@ async function getMoreSongs(url) {
   showData(data);
 }
 
+// Get lyrics for song
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, "<br>");
+
+  result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2>
+  <span>${lyrics}</span>`;
+
+  more.innerHTML = "";
+}
+
 // Event listeners
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -95,5 +77,17 @@ form.addEventListener("submit", (e) => {
     alert("Please type in a search term");
   } else {
     searchSongs(searchTerm);
+  }
+});
+
+// Get lyrics button click
+result.addEventListener("click", (e) => {
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName === "BUTTON") {
+    const artist = clickedEl.getAttribute("data-artist");
+    const songTitle = clickedEl.getAttribute("data-songtitle");
+
+    getLyrics(artist, songTitle);
   }
 });
